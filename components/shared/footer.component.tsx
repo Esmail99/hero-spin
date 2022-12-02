5107bfbc8210a11efad1b655dfa674bf5a936d5e
Esmail Elmoussel <elmoussel12@gmail.com>
2022-12-02 14:52:19 +0200

    feat(Footer): create component

---
+++ components/shared/footer.component.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="flex h-10 w-full items-center justify-center border-t">
+        <p className="flex items-center justify-center">
+          &#169; Esmail Elmoussel
+        </p>
+      </footer>
+    </>
+  );
+};
